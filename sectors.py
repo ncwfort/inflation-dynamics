a8138ee720997e7cf3e4211216e3293a8814c889
ncwfort <ncwfort@gmail.com>
2025-11-13 16:04:03 -0500

    Code review and comments for sectors.

--- sectors.py
+++ sectors.py
@@ -1,58 +1,77 @@
 from params import SectorParams
 
 class Sector:
-    """Implements the price and wage behavior of a single sector of the economy."""
+    """ Implements the price and wage behavior of a single 
+        sector of the economy. Includes storage of time series data, the 
+        functionality to advance by a period, and return time series.
+        Initialized with a params object."""
     def __init__(self, params : SectorParams):
         self.params = params
         self.wages = [params.get('w0')]
         self.prices = [params.get('p0')]
         self.wage_shares = [self.calc_wage_share()]
 
-    def calc_wage_share(self):
-        """Calculates the wage share given the most recent price and wage information."""
-        a = self.params.get('a')
-        return (self.wages[-1] * a) / self.prices[-1]
     
+    """ Updates wages and prices for a single period, according to
+        differential equation specifying the dynamics of the model."""
     def update(self):
-        """Updates wages and prices for another period."""
         # get most recent wages, prices, wage share
         last_wages = self.get_last_wages()
         last_prices = self.get_last_prices()
         last_wage_share = self.get_last_wage_share()
+        # stores global parameters
         freq_max = self.params.global_params.frequency_max
         v_f = self.params.global_params.v_f
         v_w = self.params.global_params.v_w
-        # defaults to 0
+        # defaults to last value, only changes based on logic
         new_wages = last_wages
         new_prices = last_prices
         # first, check whether wages update
         if(self.wages_update()):
-            wage_freq_fraction = self.params.get('freq_w') / freq_max
-            wage_percent_change = wage_freq_fraction * self.params.get('mu') * (v_w - last_wage_share)
+            # get the fraction by which to adjust parameter
+            freq_fraction = self.params.get('freq_w') / freq_max
+            # get the departure from aspirational value
+            diff_from_desired = v_w - last_wage_share
+            mu = self.params.get('mu')
+            # calculate wage percent change based on formulaa
+            wage_percent_change = freq_fraction * mu * diff_from_desired
             new_wages = last_wages * (1 + wage_percent_change)
         self.wages.append(new_wages)
         # next, check whether prices update
         if(self.prices_update()):
-            price_freq_fraction = self.params.get('freq_f') / freq_max
-            prices_percent_change = price_freq_fraction * self.params.get('phi') * (last_wage_share - v_f)
+            # get the fraction by which to adjust parameter
+            freq_fraction = self.params.get('freq_f') / freq_max
+            # get departure from aspiration value
+            diff_from_desired = last_wage_share - v_f
+            phi = self.params.get('mu')
+            prices_percent_change = freq_fraction * diff_from_desired * phi
             new_prices = last_prices * (1 + prices_percent_change)
         self.prices.append(new_prices)
+        # calculate new wage share and add to wage share series
         self.wage_shares.append(self.calc_wage_share())
 
-
+    """Checks whether wages update in the current period"""
     def wages_update(self):
-        """Checks whether wages update in the current period"""
+        # gets the current period by finding the length of wages vector
+        # since this includes a value at 0
         current_period = len(self.wages)
         freq = self.params.get('freq_w')
         lag = self.params.get('lag_w')
         return (current_period - lag) % freq == 0 
     
+    """Checks whether prices update in the current period."""
     def prices_update(self):
-        """Checks whether prices update in the current period."""
+        # gets the current period by finding the length of the prices vector
         current_period = len(self.prices)
         freq = self.params.get('freq_f')
         lag = self.params.get('lag_f')
         return (current_period - lag) % freq == 0
+    
+    """ Calculates the wage share (wa)/p given the data from the most recent 
+        period."""
+    def calc_wage_share(self):
+        a = self.params.get('a')
+        return (self.wages[-1] * a) / self.prices[-1]
     
     """Time series of wages."""
     def get_wage_time_series(self):
@@ -66,32 +85,38 @@ class Sector:
     def get_wage_share_time_series(self):
         return self.wage_shares
     
+    """Returns wages in last period."""
     def get_last_wages(self):
         return self.wages[-1]
     
+    """Returns prices in last period."""
     def get_last_prices(self):
         return self.prices[-1]
-
+    
+    """ Returns prices in last period. (Assumes has already been calculated
+        and that the list has been updated."""
     def get_last_wage_share(self):
         return self.wage_shares[-1]
     
+    """Consider deleting if I don't find a reference for this."""
     def get_n_periods(self):
-        # how many periods have elapse
         return len(self.wages)
     
-    """Return the equilibrium value where wages and prices change by the same amount."""
-    def get_equilibrium(self):
+    """Return the equilibrium wage share value where wages and prices change 
+        by the same amount. Obtained through analyzing the equations"""
+    def get_ws_equilibrium(self):
         mu = self.params.get('mu')
         phi = self.params.get('phi')
         return (mu * self.params.global_params.v_w + 
                 phi * self.params.global_params.v_f) / (mu + phi)
     
+    """Returns the index weight for this sector"""
     def get_index_weight(self):
-        """Returns the index weight for this sector"""
         return self.params.get('index_weight')
     
+    """ Returns this sector's contribution to the overall price index in a 
+        particular period"""
     def get_indexed_price(self, period):
-        """Returns this sector's contribution to the overall price index in a particular period"""
         weight = self.get_index_weight()
         return self.prices[period] * weight
 
