a2842aaa1357489ffdf458f63bce38efc3aa3fe8
ncwfort <ncwfort@gmail.com>
2025-11-13 16:35:07 -0500

    Reviewed code and added comments for rw.

--- rw.py
+++ rw.py
@@ -1,25 +1,39 @@
 import csv
 from params import SectorParams
 
+"""
+This module includes the methods that are used to interact with CSVs. Just
+read is implemented at present.
+"""
 
-
-"""Reads in parameters from a CSV file"""
 def get_params(filename):
+    """
+    Reads in parameters from a CSV file and returns a list of lists of
+    parameters. All parsing of these lists is handled by Economy and
+    SectorParams classes.
+
+    filename: the name of the CSV file to be read in.
+    """
     all_params = []
     with open(filename, 'r', newline = '') as csv_file:
         reader = csv.reader(csv_file)
-        next(reader)
+        next(reader) # skip header row
         for row in reader:
             all_params.append(row)
     return all_params
 
-""" Testing parameter reading. Gets a row of the CSV and feeds it into the new
-    parameters modeule that I have created."""
 def test_param_reading(filename):
+    """
+    Test function for parameter reading. Turns a row of data directly into a
+    SectorParams object and prints out the data for that object.
+
+    filename: CSV file to be read in.
+    """
     with open(filename, 'r', newline = '') as csv_file:
         reader = csv.reader(csv_file)
         next(reader)
         for row in reader:
             these_params = SectorParams(row)
             print(these_params.data)
+            print('\n')
     
